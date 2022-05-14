d7377ab68a1cff4d607421c29e307146541342f0
nikak <nikakar@gmail.com>
2022-05-14 14:59:12 +0200

    initial commit with circleci config and spring initializer

---
+++ src/main/java/org/nnn4eu/cicdcircleci/CicdCircleciApplication.java
@@ -0,0 +1,13 @@
+package org.nnn4eu.cicdcircleci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdCircleciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdCircleciApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/nnn4eu/cicdcircleci/CicdCircleciApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nnn4eu.cicdcircleci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdCircleciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
