d57358c355533d574ce31b2799387bbd67f26066
gihubme <n.karakin@qmul.ac.uk>
2022-05-23 14:12:07 +0200

    minor major bash

--- scripts/major.sh
+++ scripts/major.sh
@@ -2,4 +2,4 @@
 
 git checkout main
 mvn --batch-mode build-helper:parse-version versions:set -DnewVersion='${parsedVersion.nextMajorVersion}.0.0-SNAPSHOT' versions:commit
-mvn mvn --batch-mode clean release:prepare release:perform
+mvn --batch-mode clean release:prepare release:perform
--- scripts/minor.sh
+++ scripts/minor.sh
@@ -5,4 +5,4 @@
 git checkout main
 mvn --batch-mode build-helper:parse-version versions:set -DnewVersion='${parsedVersion.majorVersion}.${parsedVersion.nextMinorVersion}.0-SNAPSHOT' versions:commit
 #mvn deploy -s $SCRIPT_DIR/../.circleci/settings.xml
-mvn mvn --batch-mode clean release:prepare release:perform
+mvn --batch-mode clean release:prepare release:perform
